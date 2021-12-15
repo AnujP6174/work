c03f79556b7f51dba076faae1b5ed760a2461a6a
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-12-15 15:24:23 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -1,19 +1,77 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="slider.css"> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <title>About Vaccy Tours & Travels</title>
+    <style>
+        body {
+            background-image: url("https://source.unsplash.com/1600x900/?maldives,beach");
+            height: 100vh;
+            overflow: hidden;
+        }
+
+        .center {
+            margin: auto;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: auto;
+            background: whitesmoke;
+            border-radius: 10px;
+            box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.05);
+        }
+
+        .center h6 {
+            padding: 20px 0;
+            border-bottom: 1px solid silver;
+        }
+
+        .center h2 {
+            padding: 5px;
+        }
+    </style>
 </head>
 
 <body>
-    
+    <!-- Sidebar start -->
+    <div class="sidebar">
+        <a href="report"><i class="fas fa-tasks"></i><span>Progress Report</span></a>
+        <a href="about"><i class="fas fa-info-circle"></i><span>About You</span></a>
+        <a href="https://studde.com/"><i class="fas fa-video"></i><span>Lectures</span></a>
+        <a href="test"><i class="fas fa-graduation-cap"></i><span>Test Panel</span></a>
+        <a href="courses"><i class="fas fa-university"></i><span>Courses</span></a>
+        <a href="los"><i class="far fa-star"></i><span>Imp LOS</span></a>
+        <a href="logout"><i class="fas fa-sign-out-alt"></i><span>Logout</span></a>
+    </div>
+    <!-- Sidebar ends -->
+    <div class="container-fluid center">
+        <h1>About Us</h2>
+            <h6>Who we are & where do we stand</h6>
+            <div class="container-fluid">
+                <h4>Vision</h4>
+
+                <p>
+                    Keeping the core values and the ethics in center, Vaccy Tours & Travels will be a benchmark in training the youth for a better situation. Our business will work for enhancement of all good qualities in the modern youth with a brighter way…
+                </p>
+                <h4>Mission</h4>
+                <p>
+                    The motive of the our business is to moderate the young thinking for a happy and developed world. The youth become responsible and understand their own need for the society and country is the heart value of the mission…
+                </p>
+                <h4>Objectives</h4>
+                <p>
+                    The idea of establishing NGO came up during the various activities with other NGOs as a part of collaboration with NSS/NCC in college activities. It was a very clear view behind the organization that it will be for young people and specially students development activities will be given priority.
+
+                    Keeping the ethics and value system in center, the NGO has thought for such activities, which will fill valuable qualities in the youth. NGO is thankful to all the young students of various colleges who are voluntarily giving a boost to the activities. For spreading awareness in the field of adventure, NGO has planned some very charming events as per regular basis.
+                </p>
+            </div>
+    </div>
 </body>
 
 </html>
