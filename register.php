33565de4e770336510955299673c39d85f6f3c14
Anuj Panchal <53812609+AnujP6174@users.noreply.github.com>
2021-12-15 23:27:11 +0530

    Add files via upload

--- register.php
+++ register.php
@@ -6,15 +6,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css">
     <title>Vaccy Tours & Travels</title>
 </head>
 
 <body>
-    <div class="center">
-        <!-- <h2>Welcome To Vacy Tours & Travels</h2> -->
+    <!-- Navbar starts -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="top_nav">
+        <div class="container-fluid">
+            <a class="navbar-brand">Vaccy Tours & Travels</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-3 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="packages">Packages</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- Navbar ends -->
+
+    <div class="center my-4">
+        <h2>Welcome To Vacy Tours & Travels</h2>
         <h2>Register Yourself</h2>
         <form method="POST">
             <div class="txt_field">
@@ -56,7 +82,7 @@
                         $email = $_POST['Email'];
                         $password = $_POST['Password'];
                         $pw_hash = password_hash($password, PASSWORD_DEFAULT);
-                        
+
                         $stmt = $conn->prepare("INSERT INTO `register_tb`(`Name`, `Email`, `Mobile`, `Password`) VALUES (?,?,?,?)");
                         $stmt->bind_param("ssss", $full, $email, $mobile, $pw_hash);
                         $execval = $stmt->execute();
